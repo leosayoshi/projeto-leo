9e248a51a80052f6d52b5c107c7f9d72c671606c
leosayoshi01 <leosayoshi01@gmail.com>
2014-04-28 18:57:20 +0000

    update

--- php/form.php
+++ php/form.php
@@ -14,35 +14,19 @@ if (isset($_GET['message'])) {
     }
 }
 
-if (isset($_GET['idusuario']) && ($idusuario = $_GET['idusuario'])) {
-    $sql = "SELECT * FROM usuario WHERE idpessoa = $idusuario";
-    $result = mysql_query($sql, $dataBase);
-    
-    if (mysql_num_rows($result) == 1) {
-        $row = mysql_fetch_assoc($result);
-        $nome = $row['nome'];
-        $email = $row['email'];
-    }
-}
-
 ?>
-
-<?php if(isset($idusuario)): ?>
-<h1>Edicao da pessoa <?php echo $nome ?></h1>
-<?php else: ?>
-<h1>Nova pessoa</h1>
-<?php endif; ?>
-
-<form action="php/process.php<?php echo isset($idusuario) ? '?idusuario=' . $idusuario : '' ?>" method="post">
+<div id="cadastro01">
+<form id="cadastro" action="php/process.php" class="cadastro" method="post">
+    <fieldset> <legend>Novo Cadastro</legend>
     <label for="nome">Nome</label>
-    <input type="text" name="nome" id="nome" value="<?php echo isset($nome) ? $nome : '' ?>" />
+    <input type="text" name="nome" id="nome" />
 
     <label for="email">E-mail</label>
-    <input type="email" name="email" id="email" value="<?php echo isset($email) ? $email : '' ?>" />
+    <input type="email" name="email" id="email" />
 
     <label for="celular">Senha</label>
     <input type="password" name="senha" id="senha" maxlength="11" />
-
+    </fieldset>
     <input type="submit" value="Salvar" />
-	<a href="index.php" title="Cancelar">Cancelar</a>
 </form>
+</div>
