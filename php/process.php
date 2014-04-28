5961f610bb5270b2d00e71a63e158414f05c4fa4
leosayoshi01 <leosayoshi01@gmail.com>
2014-04-28 18:45:00 +0000

    update

--- php/process.php
+++ php/process.php
@@ -4,12 +4,14 @@ include 'connection.php';
 
 $nome = isset($_POST['nome']) && $_POST['nome'] ? trim($_POST['nome']) : null;
 $email = isset($_POST['email']) && $_POST['email'] ? trim($_POST['email']) : null;
-$celular = isset($_POST['celular']) && $_POST['celular'] ? trim($_POST['celular']) : null;
+$senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : null;
 
-if ($nome && $email && $celular) {
-    $sql = (isset($_GET['idpessoa']) && ($idpessoa = $_GET['idpessoa'])) 
-            ? "UPDATE pessoa SET nome='$nome', email='$email', celular='$celular' WHERE idpessoa = $idpessoa" 
-            : "INSERT INTO pessoa (nome, email, celular) VALUES ('$nome', '$email', '$celular')";
+if ($nome && $email && $senha) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL))
+    $senha = md5($_POST['senha']);
+    $sql = (isset($_GET['idusuario']) && ($idusuario = $_GET['idusuario'])) 
+            ? "UPDATE usuario SET nome='$nome', email='$email', senha='$senha' WHERE idusuario = $idusuario" 
+            : "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
     $result = mysql_query($sql, $dataBase);
 
