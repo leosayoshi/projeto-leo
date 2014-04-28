63d7a9e321803efb21ec1f9844ea8810c75cf68e
leosayoshi01 <leosayoshi01@gmail.com>
2014-04-28 05:14:54 +0000

    update

---
+++ php/home.php
@@ -0,0 +1,5 @@
+<div id="home">
+<h4>Seja bem vindo!</h4>
+
+<p><?php echo date('d/m/Y') ?></p>
+</div>
---
+++ php/naopermitida.php
@@ -0,0 +1,3 @@
+<h1>Para ver o conteúdo desta página você precisa estar logado.</h4>
+
+<a href="index.php" title="Página inicial">Página inicial</a>
