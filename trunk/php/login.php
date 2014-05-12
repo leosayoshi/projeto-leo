6e7184a5f85fe6c48230210f5ca220d11bd7d7ac
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-05-12 02:38:15 +0000

    sistema de login feito com nivel de permissao

--- trunk/php/login.php
+++ trunk/php/login.php
@@ -6,16 +6,17 @@ $email = isset($_POST['email']) && $_POST['email'] ? trim($_POST['email']) : nul
 $senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : null;
 
 if ($email && $senha) {
-	$sql = "SELECT * FROM usuario WHERE email = '$email' AND senha = MD5('$senha')";
+	$sql = "SELECT * FROM cadastro WHERE email = '$email' AND senha = MD5('$senha')";
 	$result = mysql_query($sql, $dataBase);
 	if (mysql_num_rows($result) == 1) {
 		session_start();
 		$row = mysql_fetch_assoc($result);
 		$_SESSION['email'] = $row['email'];
+		$_SESSION['tipo'] = $row['tipo'];
 				
 		if (isset($_POST['manterLogado']) && $_POST['manterLogado'] == '1') {
-			setcookie('manterLogado', 1, time()+60*60, '/site/');
-			setcookie('email', $row['email'], time()+60*60, '/site/');
+			setcookie('manterLogado', 1, time()+60*60, '/trunk/');
+			setcookie('email', $row['email'], time()+60*60, '/trunk/');
 		}
 			
 		header('Location: ../index.php');
