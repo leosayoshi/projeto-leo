92cbeeead3fd5449961246294c3cd1d331d9788f
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-05-12 23:32:04 +0000

    variavel "$tipo" alterada para "$especializacao" para adaptar ao campo especializacao do formulario

--- trunk/php/processServico.php
+++ trunk/php/processServico.php
@@ -7,15 +7,15 @@ $email = isset($_POST['email']) && $_POST['email'] ? trim($_POST['email']) : nul
 $senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : null;
 $rua = isset($_POST['rua']) && $_POST['rua'] ? trim($_POST['rua']) : null;
 $bairro = isset($_POST['bairro']) && $_POST['bairro'] ? trim($_POST['bairro']) : null;
-$tipo = isset($_POST['tipo']) && $_POST['tipo'] ? trim($_POST['tipo']) : null;
 $servico = isset($_POST['servico']) && $_POST['servico'] ? trim($_POST['servico']) : null;
+$especializacao = isset($_POST['especializacao']) && $_POST['especializacao'] ? trim($_POST['especializacao']) : null;
 
-if ($nome && $email && $senha && $rua && $bairro && $tipo && $servico) {
+if ($nome && $email && $senha && $rua && $bairro && $tipo && $especializacao) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL))
     $senha = md5($_POST['senha']);
     $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) 
-            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha', rua=$rua, bairro=$bairro, tipo=$tipo, servico=$servico WHERE idcadastro = $idcadastro" 
-            : "INSERT INTO cadastro (nome, email, senha, rua, bairro, tipo, servico) VALUES ('$nome', '$email', '$senha', '$rua', '$bairro', '$tipo', '$servico')";
+            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha', rua=$rua, bairro=$bairro, servico=$servico, especializacao=$especializacao WHERE idcadastro = $idcadastro" 
+            : "INSERT INTO cadastro (nome, email, senha, rua, bairro, tipo, servico) VALUES ('$nome', '$email', '$senha', '$rua', '$bairro', '$servico', '$especializacao')";
 
     $result = mysql_query($sql, $dataBase);
 
