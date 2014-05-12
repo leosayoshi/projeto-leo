fb6bfc373072ac0b1b1b9e571843e54b3b9df221
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-05-12 02:35:50 +0000

    nome do banco de dado alterado para a versao do banco feita em sala de aula

--- trunk/php/connection.php
+++ trunk/php/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 $dataBase = mysql_connect('localhost', 'root', 'kyosetsu');
-$base = mysql_select_db('buscamedrio');
+$base = mysql_select_db('buscamedica');
 
 if (!$dataBase && !$base) 
     die('<p>Ocorreu um problema ao tentar conectar com o banco de dados .</p>');
