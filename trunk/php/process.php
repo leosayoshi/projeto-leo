ac1b2292d3dd9f38812a9d8150cb92581fdad819
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-05-13 03:14:26 +0000

    tipo 1 = paciente alterado para ser salvo no banco de dados ao se cadastrar no sistema

--- trunk/php/process.php
+++ trunk/php/process.php
@@ -9,9 +9,10 @@ $senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : nul
 if ($nome && $email && $senha) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL))
     $senha = md5($_POST['senha']);
+    $tipo = 1;
     $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) 
-            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha' WHERE idcadastro = $idcadastro" 
-            : "INSERT INTO cadastro (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha', tipo='$tipo' WHERE idcadastro = $idcadastro" 
+            : "INSERT INTO cadastro (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha', '$tipo')";
 
     $result = mysql_query($sql, $dataBase);
 
