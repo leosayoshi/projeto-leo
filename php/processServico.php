36ede2c75eb8c7572e3212b1287695eb12d276a6
leosayoshi01 <leosayoshi01@gmail.com>
2014-05-25 16:07:51 +0000

    arquivo process servico atualizado com novas validações

--- php/processServico.php
+++ php/processServico.php
@@ -5,17 +5,15 @@ include 'connection.php';
 $nome = isset($_POST['nome']) && $_POST['nome'] ? trim($_POST['nome']) : null;
 $email = isset($_POST['email']) && $_POST['email'] ? trim($_POST['email']) : null;
 $senha = isset($_POST['senha']) && $_POST['senha'] ? trim($_POST['senha']) : null;
-$rua = isset($_POST['rua']) && $_POST['rua'] ? trim($_POST['rua']) : null;
-$bairro = isset($_POST['bairro']) && $_POST['bairro'] ? trim($_POST['bairro']) : null;
-$servico = isset($_POST['servico']) && $_POST['servico'] ? trim($_POST['servico']) : null;
-$especializacao = isset($_POST['especializacao']) && $_POST['especializacao'] ? trim($_POST['especializacao']) : null;
 
-if ($nome && $email && $senha && $rua && $bairro && $tipo && $especializacao) {
+
+if ($nome && $email && $senha) {
     if (filter_var($email, FILTER_VALIDATE_EMAIL))
     $senha = md5($_POST['senha']);
+    $tipo = 2;
     $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) 
-            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha', rua=$rua, bairro=$bairro, servico=$servico, especializacao=$especializacao WHERE idcadastro = $idcadastro" 
-            : "INSERT INTO cadastro (nome, email, senha, rua, bairro, tipo, especializacao) VALUES ('$nome', '$email', '$senha', '$rua', '$bairro', '$servico', '$especializacao')";
+            ? "UPDATE cadastro SET nome='$nome', email='$email', senha='$senha' WHERE idcadastro = $idcadastro" 
+            : "INSERT INTO cadastro (nome, email, senha, tipo) VALUES ('$nome', '$email', '$senha','$tipo')";
 
     $result = mysql_query($sql, $dataBase);
 
