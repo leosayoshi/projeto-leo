bbd4697268372e56b00cc0f9195b1720e7b13800
leosayoshi01 <leosayoshi01@gmail.com>
2014-06-01 02:17:42 +0000

    busca incrementada com bairro

--- php/busca.php
+++ php/busca.php
@@ -1,4 +1,18 @@
 <div id="busca">
-    
+    <form>
+    <fieldset id="busca">Busca
+            <fieldset>Especializacao</fieldset>
+            <fieldset>Bairro
+            <select> <?php
+$sql = mysql_query("SELECT nome FROM bairro");
+while($nome = mysql_fetch_object($sql)){
+echo "<option value='$nome->nome'>
+	$nome->nome
+	</option>";
+}
+?></fieldset></select>
+            <input type="submit" value="buscar">
+    </fieldset>
+    </form>
     
 </div>
