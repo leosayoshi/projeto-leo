9359cd02a4cdb495eeedafc425e080fd9554c8a1
leosayoshi01 <leosayoshi01@gmail.com>
2014-06-30 20:21:35 +0000

    layout da pagina de busca com os campos que podem ser usados na pesquisa iniciados
    started issue 6

--- php/busca.php
+++ php/busca.php
@@ -1,8 +1,27 @@
 <div id="busca">
-    <form>
+    <form id="busca">
     <fieldset id="busca">Busca
-            <fieldset>Especializacao</fieldset>
-            <fieldset>Bairro
+        <fieldset>
+                        <label for="servico">Servico</label> 
+                       <?php $sql = mysql_query("SELECT * FROM servico");
+while($servico = mysql_fetch_object($sql)){
+echo "<input type='radio' name='idservico' value='$servico->idservico'/>
+	$servico->nome";
+}
+?>
+                        <br/>
+                        </fieldset>
+                             <fieldset><legend> Especializacao</legend>
+           <?php $sql = mysql_query("SELECT * FROM especializacao");
+while($especializacao = mysql_fetch_object($sql)){
+echo "<input type='checkbox' name='cadastroespecializacao[]'  value='$especializacao->idespecializacao'/>
+	$especializacao->nome";
+}
+?>
+        <br/>
+                        </fieldset>
+                        <fieldset>
+          <legend>Bairro</legend>
             <select> <?php
 $sql = mysql_query("SELECT nome FROM bairro");
 while($nome = mysql_fetch_object($sql)){
@@ -10,8 +29,9 @@ echo "<option value='$nome->nome'>
 	$nome->nome
 	</option>";
 }
-?></fieldset></select>
+?></select>
             <input type="submit" value="buscar">
+    </fieldset>
     </fieldset>
     </form>
     
