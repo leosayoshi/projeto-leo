ac3a9e5fe35d6b822009e23b137a7fca3d7d4c99
leosayoshi01 <leosayoshi01@gmail.com>
2014-07-03 20:06:16 +0000

    iniciado implementação da home do site mostrando os 3 ultimos serviços médicos cadastrados no sistema
    issue 13

--- php/home.php
+++ php/home.php
@@ -2,4 +2,30 @@
 <h4>Seja bem vindo!</h4>
 
 <p><?php echo date('d/m/Y') ?></p>
+<div id="propServ">
+    <table cellspacing="0" border="0">
+	<tr class="title">
+		<th>Nome do Servico</th>
+		<th>Tipo do servico</th>
+		<th>Endereco</th>
+		<th>Bairro</th>
+	</tr>
+    <?php
+    $sql = 'SELECT * FROM cadastro WHERE idcadastro=1';
+	$result = mysql_query($sql, $dataBase);
+
+	if ($result && mysql_num_rows($result) > 0) {
+		while ($row = mysql_fetch_assoc($result)) {
+			echo '<tr>';
+			echo '<td>' . $row['nome'] . '</td>';
+			echo '<td>' . $row['idservico'] . '</td>';
+			echo '<td>' . $row['endereco'] . '</td>';
+			echo '<td>' . $row['idbairro'] . '</td>';
+			echo '</tr>';
+		}
+	} else
+		echo '<tr><td cols="4">Nao tem pessoas cadastradas.</td></tr>';
+	?>
+    </table>
+</div>
 </div>
