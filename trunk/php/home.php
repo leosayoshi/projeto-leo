7269cf5197f7613e6057adfa2f74af2ee8ede65b
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-07-15 04:29:36 +0000

    atualizada código onde aparecem corretamente dentro da div os nome do bairro e o serviço dos 3 ultimos cadastros feitos no sistema issue 13

--- trunk/php/home.php
+++ trunk/php/home.php
@@ -4,15 +4,27 @@
 <p><?php echo date('d/m/Y') ?></p>
 
     <?php
-    $sql = 'SELECT * FROM cadastro WHERE idcadastro ORDER BY idcadastro DESC LIMIT 3';
+    $sql = 'SELECT c.*, b.nome as nomeBairro, s.nome as nomeServico
+	FROM cadastro c
+	JOIN bairro b ON c.idbairro = b.idbairro
+	JOIN servico s ON c.idservico = s.idservico
+
+	WHERE  c.tipo="2" ORDER BY c.idcadastro DESC LIMIT 3';
+
 	$result = mysql_query($sql, $dataBase);
 
 	if ($result && mysql_num_rows($result) > 0) {
-		while ($row = mysql_fetch_assoc($result)) {
-			echo '<div style="border: 2px solid blue; color: margin: 3px; padding: 2px; width: 30%;">'. ' Servico: '.$row['nome'] .' servico ' . $row['idservico'] .'<br/>'.' Especilzacao'.$row['especializacao'].'<br/>'.' Endereco '.' '. $row['endereco'] . '<br/>'.' Bairro '. $row['idbairro'] .'</div>'.'<br/>';
-
-		}
-	} else
-		echo '<tr><td cols="4">Nao tem pessoas cadastradas.</td></tr>';
-	?>  
+    while ($row = mysql_fetch_assoc($result)) {
+        echo '<div style="border: 2px solid blue; color: margin: 3px; padding: 2px; width: 30%;">';
+        echo ' Nome: ' . $row['nome'] . '<br/>';
+        echo ' Servico: ' . $row['nomeServico'] . '<br/>';
+        echo ' Especializacao: ' . '<br/>';
+        echo ' Endereco ' . $row['endereco'] . '<br/>';
+        echo ' Bairro ' . $row['nomeBairro'] . '<br/>';
+        echo '</div>' . '<br/>';
+    }
+} else {
+    echo '<tr><td cols="4">Nao tem pessoas cadastradas.</td></tr>';
+}
+?>  
 </div>
