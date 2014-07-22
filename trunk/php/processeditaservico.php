7b33ea2148d71ab45bc6872141ed51f85de5076f
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-07-22 21:26:45 +0000

    revisado funcionamento da edição de cadastro
    fixed issue 9

--- trunk/php/processeditaservico.php
+++ trunk/php/processeditaservico.php
@@ -5,14 +5,13 @@ $nome = $_POST['nome'];
 $senha = $_POST['senha'];
 $endereco = $_POST['endereco'];
 $idbairro = $_POST['idbairro'];
-$idservico = $_POST['idservico'];
-$cadastroespecializacao = $_POST['$cadastroespecializacao'];
 
 
-if ($nome || $senha || $endereco || $idbairro ||$idservico ||$cadastroespecializacao) {
+
+if ($nome || $senha || $endereco || $idbairro) {
   
         
-        $result = mysql_query($sql, $dataBase);
+        $result = mysql_query($sql, $dataBase);{
 
         if (mysql_num_rows($result) == 0) {
             if (strlen($senha) >= 0 || 6) {
@@ -22,16 +21,9 @@ if ($nome || $senha || $endereco || $idbairro ||$idservico ||$cadastroespecializ
             exit;
             }
             $tipo = 2;
-        $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) ? "UPDATE cadastro SET nome='$nome', senha='$senha', tipo='$tipo' idbairro='$idbairro', endereco='$endereco', idservico='$idservico' WHERE idcadastro = $idcadastro" : "INSERT INTO cadastro (nome, senha, tipo, bairro, endereco, idservico) VALUES ('$nome', '$senha', '$tipo','$bairro','$endereco', '$idservico')";
+        $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) ? "UPDATE cadastro SET nome='$nome', senha='$senha', tipo='$tipo' idbairro='$idbairro', endereco='$endereco' WHERE idcadastro = $idcadastro" : "INSERT INTO cadastro (nome, senha, tipo, idbairro, endereco) VALUES ('$nome', '$senha', '$tipo','$idbairro','$endereco')";
 
         $result = mysql_query($sql, $dataBase);
-        if ($result) {
-                $idcadastro = mysql_insert_id();
-                foreach ($cadastroespecializacao as $idespecializacao) {
-                    $sql = "INSERT INTO cadastroespecializacao (idcadastro, idespecializacao) 
-					VALUES ($idcadastro, $idespecializacao)";
-                    $result = mysql_query($sql, $dataBase);
-                }
 
         if ($result) {
             header('Location: ../index.php?pagina=home&message=3');
