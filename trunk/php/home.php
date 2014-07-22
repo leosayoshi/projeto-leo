555856b7a10250fe4370a72e4aa4c5d2a6165e0c
leosayoshi01@gmail.com <leosayoshi01@gmail.com@110f31e0-69fb-73b6-d699-5cc6c85f94ce>
2014-07-22 16:24:03 +0000

    formulario, funções php e querry de busca implementados, assim como css da pagina
    issue 6

--- trunk/php/home.php
+++ trunk/php/home.php
@@ -1,7 +1,7 @@
 <div id="home">
-<h4>Seja bem vindo!</h4>
+    <h4 style="margin-left: 40%;">Seja bem vindo!</h4>
 
-<p><?php echo date('d/m/Y') ?></p>
+<p style="margin-left: 40%;"><?php echo date('d/m/Y') ?></p>
 
     <?php
     $sql = 'SELECT c.*, b.nome as nomeBairro, s.nome as nomeServico
@@ -9,13 +9,13 @@
 	JOIN bairro b ON c.idbairro = b.idbairro
 	JOIN servico s ON c.idservico = s.idservico
 
-	WHERE  c.tipo="2" ORDER BY c.idcadastro DESC LIMIT 3';
+	WHERE  c.tipo="2" ORDER BY c.idcadastro DESC LIMIT 5';
 
 	$result = mysql_query($sql, $dataBase);
 
 	if ($result && mysql_num_rows($result) > 0) {
     while ($row = mysql_fetch_assoc($result)) {
-        echo '<div style="border: 2px solid blue; color: margin: 3px; padding: 2px; width: 30%;">';
+        echo '<div style="border: 2px solid black; margin-left:30%; background: rgba(255,255,255,.9); color: margin: 3px; padding: 2px; width: 30%;">';
         echo ' Nome: ' . $row['nome'] . '<br/>';
         echo ' Servico: ' . $row['nomeServico'] . '<br/>';
         echo ' Especializacao: ' . '<br/>';
