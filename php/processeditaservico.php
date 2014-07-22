1db8cf02c51d3494acb73c615e748a4738a259fd
leosayoshi01 <leosayoshi01@gmail.com>
2014-07-22 21:18:29 +0000

    revisado funcionamento da edição de cadastro
    fixed issue 9

--- php/processeditaservico.php
+++ php/processeditaservico.php
@@ -5,38 +5,48 @@ $nome = $_POST['nome'];
 $senha = $_POST['senha'];
 $endereco = $_POST['endereco'];
 $idbairro = $_POST['idbairro'];
+$idservico = $_POST['idservico'];
+$cadastroespecializacao = $_POST['$cadastroespecializacao'];
 
-if ($nome || $senha || $endereco || $idbairro) {
+
+if ($nome || $senha || $endereco || $idbairro ||$idservico ||$cadastroespecializacao) {
   
         
-        $result = mysql_query($sql, $dataBase);{
+        $result = mysql_query($sql, $dataBase);
 
         if (mysql_num_rows($result) == 0) {
             if (strlen($senha) >= 0 || 6) {
                 $senha = md5($_POST['senha']);
             } else {
-                 header('Location: ../index.php?pagina=formeditapaciente&message=5');
+                 header('Location: ../index.php?pagina=formeditaservico&message=5');
             exit;
             }
-            $tipo = 1;
-        $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) ? "UPDATE cadastro SET nome='$nome', senha='$senha', tipo='$tipo' bairro='$idbairro', endereco='$endereco' WHERE idcadastro = $idcadastro" : "INSERT INTO cadastro (nome, senha, tipo, bairro, endereco) VALUES ('$nome', '$senha', '$tipo','$idbairro','$endereco')";
+            $tipo = 2;
+        $sql = (isset($_GET['idcadastro']) && ($idcadastro = $_GET['idcadastro'])) ? "UPDATE cadastro SET nome='$nome', senha='$senha', tipo='$tipo' idbairro='$idbairro', endereco='$endereco', idservico='$idservico' WHERE idcadastro = $idcadastro" : "INSERT INTO cadastro (nome, senha, tipo, bairro, endereco, idservico) VALUES ('$nome', '$senha', '$tipo','$bairro','$endereco', '$idservico')";
 
         $result = mysql_query($sql, $dataBase);
+        if ($result) {
+                $idcadastro = mysql_insert_id();
+                foreach ($cadastroespecializacao as $idespecializacao) {
+                    $sql = "INSERT INTO cadastroespecializacao (idcadastro, idespecializacao) 
+					VALUES ($idcadastro, $idespecializacao)";
+                    $result = mysql_query($sql, $dataBase);
+                }
 
         if ($result) {
             header('Location: ../index.php?pagina=home&message=3');
             exit;
         } else {
-            header('Location: ../index.php?pagina=formeditapaciente&message=2');
+            header('Location: ../index.php?pagina=formeditaservico&message=2');
             exit;
         }
         } else {
-             header('Location: ../index.php?pagina=formeditapaciente&message=4');
+             header('Location: ../index.php?pagina=formeditaservico&message=4');
                 exit;
         }
     }
 } else {
-    header('Location: ../index.php?pagina=formeditapaciente&message=1');
+    header('Location: ../index.php?pagina=formeditaservico&message=1');
     exit;
 }
 ?>
